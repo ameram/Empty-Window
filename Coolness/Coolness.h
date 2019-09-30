4c3622dd2eb15c060a7ec7b5e4d9584a33959751
Amir <shopdoup@yahoo.com>
2019-09-30 17:34:25 +0330

    New framework added.

---
+++ Coolness/Coolness.h
@@ -0,0 +1,19 @@
+//
+//  Coolness.h
+//  Coolness
+//
+//  Created by Amirmohamad on 9/30/19.
+//  Copyright © 2019 AMR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Coolness.
+FOUNDATION_EXPORT double CoolnessVersionNumber;
+
+//! Project version string for Coolness.
+FOUNDATION_EXPORT const unsigned char CoolnessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coolness/PublicHeader.h>
+
+
