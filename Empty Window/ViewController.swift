6b01a91348fdd85e16b056ab80a362a8c13a8e49
Amir <shopdoup@yahoo.com>
2019-09-30 10:18:06 +0330

    Initial Commit

---
+++ Empty Window/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Empty Window
+//
+//  Created by Amirmohamad on 9/29/19.
+//  Copyright © 2019 AMR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
