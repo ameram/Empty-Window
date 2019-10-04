9f9ed467ffd1232c15845d551664944668cac911
Amir <shopdoup@yahoo.com>
2019-10-04 11:51:53 +0330

    Add an alert action.

--- Empty Window/ViewController.swift
+++ Empty Window/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Hello", message: "Welcome to my app! Howdy?\n I'm Amir.", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Quit", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Get in", style: .default, handler: nil))
+        self.present(alert, animated: true)
+        
+    }
+    
 }
 
